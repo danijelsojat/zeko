545f0c5d145dd213867b4f156c8d91dff6ac2fee
Danijel <danijel.sojat@gmail.com>
2023-02-25 09:13:27 +0100

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    val rabbitLine1 = "(\\(\\"
+    val rabbitLine2 = "(-.-)"
+    val rabbitLine3 = "o_(\")(\")"
+
+    println(rabbitLine1)
+    println(rabbitLine2)
+    println(rabbitLine3)
+}
